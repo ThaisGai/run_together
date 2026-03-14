dc59d06418b4a0b7ffd16b3ae6c5de80cde88b29
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-03-14 14:43:35 -0300

    corrigindo as migrations

--- db/migrate/20260314173854_create_run_members.rb
+++ db/migrate/20260314173854_create_run_members.rb
@@ -1,10 +1,12 @@
-class CreateRunMembers < ActiveRecord::Migration[8.0]
+class CreateRunMembers < ActiveRecord::Migration[7.0]
   def change
     create_table :run_members do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :run, null: false, foreign_key: true
+      t.references :run,  null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :run_members, [:user_id, :run_id], unique: true
   end
 end
--- db/migrate/20260314173855_create_chats.rb
+++ db/migrate/20260314173855_create_chats.rb
@@ -1,4 +1,4 @@
-class CreateChats < ActiveRecord::Migration[8.0]
+class CreateChats < ActiveRecord::Migration[7.0]
   def change
     create_table :chats do |t|
       t.references :run, null: false, foreign_key: true
