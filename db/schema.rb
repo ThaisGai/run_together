ab517ca85f98767e19f699f9e48f4a0c03d60344
Thais Almeida Gai <thais.gabriel@gmail.com>
2026-03-17 20:40:04 -0300

    resolvendo conflitos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2026_03_17_230443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_230443) do
   create_table "messages", force: :cascade do |t|
     t.bigint "chat_id", null: false
     t.bigint "user_id", null: false
-    t.text "content", null: false
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["chat_id"], name: "index_messages_on_chat_id"
@@ -38,7 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_230443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["run_id"], name: "index_run_members_on_run_id"
-    t.index ["user_id", "run_id"], name: "index_run_members_on_user_id_and_run_id", unique: true
+    t.index ["user_id"], name: "index_run_members_on_user_id"
   end
 
   create_table "runs", force: :cascade do |t|
