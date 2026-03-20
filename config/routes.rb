003ce1d7f0fe3bffa438d5e748982b58db60835b
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-03-19 21:25:25 -0300

    criei navbar,routes.rb, runs_controller, aplication html, criei tambem o file de my runs em view

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,10 @@
 Rails.application.routes.draw do
-
-  resources :runs #, only: %i[show new create edit update destroy]
+  resources :runs
 
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "my_runs", to: "runs#my_runs", as: :my_runs
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  #
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_03_17_230443) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_17_230443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_17_230443) do
     t.date "date"
     t.time "time"
     t.string "location"
-    t.string "pace"
+    t.string "place"
     t.boolean "private"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
