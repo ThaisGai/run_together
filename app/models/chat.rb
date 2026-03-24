6e5349f728ad83aafe51c1339a79abf887d54ebd
Beatriz Barguil <beatrizz.barguil@gmail.com>
2026-03-24 19:36:31 -0300

    atualizado model e controller de run, chat, message e show de run, show de chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,18 @@
 class Chat < ApplicationRecord
-  belongs_to :run
+  belongs_to :sender, class_name: "User"
+  belongs_to :receiver, class_name: "User"
 
   has_many :messages, dependent: :destroy
+
+  validates :sender_id, uniqueness: { scope: :receiver_id }
+
+  def self.between(user_a, user_b)
+    where(sender: user_a, receiver: user_b)
+      .or(where(sender: user_b, receiver: user_a))
+      .first || create!(sender: user_a, receiver: user_b)
+  end
+
+  def other_user(current_user)
+    current_user == sender ? receiver : sender
+  end
 end
